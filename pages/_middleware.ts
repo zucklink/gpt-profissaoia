1ab71e6651ae6e5de56b61b1787abd5ba895c0f9
Leandro F <leandrosilvaferreira@gmail.com>
2024-02-27 00:47:51 -0400

    capibara

---
+++ pages/_middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const AUTHORIZED_URLS = ['https://profissao-ia.memberz.com.br'];
+
+function isUrlAuthorized(referrer: string | null): boolean {
+  return AUTHORIZED_URLS.some(authorizedUrl => referrer?.includes(authorizedUrl));
+}
+
+export function middleware(request: NextRequest) {
+  if (process.env.NODE_ENV === 'production') {
+    const referrer = request.headers.get('referer');
+    const isInIframe = request.headers.get('sec-fetch-dest') === 'iframe';
+
+    if (isInIframe && referrer) {
+      if (!isUrlAuthorized(referrer)) {
+        return Response.json(
+          { success: false, message: 'Acesso não autorizado!' },
+          { status: 403 }
+        )
+      }
+    }
+  }
+
+  return NextResponse.next();
+}
